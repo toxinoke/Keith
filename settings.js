473dcb496d7c546b4e3c02498f5ae13c8c9f48aa
toxinoke <toxinogee@gmail.com>
2026-06-05 07:53:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,24 +7,24 @@ if (fs.existsSync('set.env')) {
 }
 
 const session = process.env.SESSION || '';
-const dev = process.env.OWNER_NUMBER || '254748387615';
+const dev = process.env.OWNER_NUMBER || '254798953494';
 
 const autostatusAutoviewStatus = process.env.AUTOVIEW_STATUS || 'true';
 const autostatusAutoLikeStatus = process.env.AUTOLIKE_STATUS || 'false';
 const autostatusAutoReplyStatus = process.env.AUTOREPLY_STATUS || 'false';
-const autostatusStatusReplyText = process.env.STATUS_REPLY_TEXT || '✅ Status Viewed By Keith Md';
+const autostatusStatusReplyText = process.env.STATUS_REPLY_TEXT || '✅ Status Viewed By Toxino md';
 const autostatusStatusLikeEmojis = process.env.STATUS_LIKE_EMOJIS || '🫟,🫟';
 
 const botPrefix = process.env.PREFIX || ".";
-const botAuthor = process.env.OWNER_NAME || "Keith";
+const botAuthor = process.env.OWNER_NAME || "Toxino";
 const botexpiration = process.env.BOT_EXPIRATION_DATE || "11/03/2027";
 const botUrl = process.env.BOT_PIC || "https://i.ibb.co/PsZnb9NH/0d81ac6b1fb1.jpg";
-const botGurl = process.env.BOT_GURL || "https://github.com/Keithkeizzah/KEITH-MD";
+const botGurl = process.env.BOT_GURL || "https://github.com/toxinoke/Toxino-MD";
 const botTimezone = process.env.BOT_TIMEZONE || "Africa/Nairobi";
-const botBotname = process.env.BOTNAME || "KEITH-MD";
-const botPackname = process.env.BOT_PACKNAME || "KEITH-MD";
+const botBotname = process.env.BOTNAME || "Toxino-MD";
+const botPackname = process.env.BOT_PACKNAME || "Toxino";
 const botMode = process.env.BOT_MODE || "public";
-const botSessionName = process.env.BOT_SESSION_NAME || "keith-md";
+const botSessionName = process.env.BOT_SESSION_NAME || "Toxino-md";
 const autosocialdownload = process.env.AUTO_SOCIAL_DOWNLOAD || "false";
 
 const { Sequelize } = require('sequelize'); 
